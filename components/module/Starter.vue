a55a10ba1b8e2d53b3d4fd93198bb051b06a0931
Chris <hello@chrispreisler.com>
2021-04-18 14:58:16 +0200

    Remove animations

--- components/module/Starter.vue
+++ components/module/Starter.vue
@@ -5,91 +5,17 @@
         {{ blok.title }}
       </BaseHeadline>
     </LayoutContainer>
-    <div
-      ref="target"
-      class="flex items-center justify-center w-full h-screen overflow-hidden"
-      style="transform: scaleX(0.86)"
-    >
-      <BaseImage ref="targetImage" :blok="blok.image" is-cover />
-    </div>
+    <BaseImage :blok="blok.image" is-cover />
   </section>
 </template>
 
 <script>
-import { ref } from "@nuxtjs/composition-api";
-import { useScroll } from "@vueuse/gesture";
-import { useIntersectionObserver } from "@vueuse/core";
-import { useMotionProperties, useSpring } from "@vueuse/motion";
-import { interpolate } from "popmotion";
-
 export default {
   props: {
     blok: {
       type: Object,
       default: () => {},
     },
-  },
-  setup() {
-    const target = ref();
-    const targetImage = ref();
-    const targetIsVisible = ref(false);
-
-    const { motionProperties } = useMotionProperties(target, {
-      scaleX: 0.86,
-    });
-    const mapper = interpolate([0, 1], [0.86, 1]);
-    const { set } = useSpring(motionProperties, {
-      damping: 50,
-      stiffness: 1000,
-    });
-
-    const { motionProperties: motionPropertiesImage } = useMotionProperties(
-      targetImage,
-      {
-        scaleX: 1 / 0.86,
-      }
-    );
-    const mapperImage = interpolate([0, 1], [1 / 0.86, 1]);
-    const { set: setImage } = useSpring(motionPropertiesImage, {
-      damping: 50,
-      stiffness: 1000,
-    });
-
-    useIntersectionObserver(
-      target,
-      ([{ isIntersecting }], observerElement) => {
-        targetIsVisible.value = isIntersecting;
-      },
-      {
-        threshold: 0,
-      }
-    );
-
-    useScroll(
-      ({ xy: [x, y], ...state }) => {
-        if (!targetIsVisible.value) {
-          return;
-        }
-
-        set({
-          scaleX: mapper(y / (document.body.clientHeight - window.innerHeight)),
-        });
-
-        setImage({
-          scaleX: mapperImage(
-            y / (document.body.clientHeight - window.innerHeight)
-          ),
-        });
-      },
-      {
-        domTarget: window,
-      }
-    );
-    return {
-      target,
-      targetIsVisible,
-      targetImage,
-    };
   },
 };
 </script>
