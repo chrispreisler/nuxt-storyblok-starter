14b5d50b74754445958b6de0dc77e979bdbbfb6e
Chris <hello@chrispreisler.com>
2021-05-10 08:46:04 +0200

    Revert "Fix small styling bug"
    This reverts commit 6768aefde94100b067bf8bcc48fb67c601eebb27.

--- components/base/Image.vue
+++ components/base/Image.vue
@@ -8,7 +8,7 @@
     :style="'aspect-ratio: ' + imageWidth / imageHeight"
     :alt="blok.alt"
     class="transition-opacity duration-500 opacity-0"
-    :class="[{ 'object-cover w-full h-full': isCover && !blok.focus }]"
+    :class="{ 'object-cover w-full h-full max-w-none': isCover && !blok.focus }"
   />
 </template>
 
@@ -86,6 +86,7 @@ export default defineComponent({
     function getTransformationPoints() {
       const focalX = props.blok.focus.split(":").pop().split("x")[0];
       const focalY = props.blok.focus.split("x").pop().split(":")[0];
+
       const wrapperWidth = image.value.parentElement.clientWidth;
       const wrapperHeight = image.value.parentElement.clientHeight;
       const newImageHeight =
@@ -171,7 +172,7 @@ export default defineComponent({
 
 <style scoped >
 .portrait {
-  @apply max-h-full w-auto max-w-none;
+  @apply max-h-full w-auto;
   height: inherit;
 }
 </style>
